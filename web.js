d0799079a5660c4fba29d5e99fed3b4d338996e2
Amey Patil <theknight374121@gmail.com>
2013-07-09 03:23:50 +0530

    A little update

--- web.js
+++ web.js
@@ -2,9 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-fs = require('fs');
-var content ;
-var data;
+var fs  = require('fs');
 fs.readFile('/home/sensei/bitstarter/index.html', 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
