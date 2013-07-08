f2427765df12e10a15c35dc1f9438861c4a8619c
Amey Patil <theknight374121@gmail.com>
2013-07-09 02:14:52 +0530

    Index added and web modified

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var content = buf.toString(fs.readFileSync('/home/bitstarter/index.html',));
+
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
