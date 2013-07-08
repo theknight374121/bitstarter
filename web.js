4f9c5a43bad92215a0ddf433dcbf91cb984b72d9
Amey Patil <theknight374121@gmail.com>
2013-07-09 03:15:45 +0530

    A little update

--- web.js
+++ web.js
@@ -9,14 +9,14 @@ fs.readFile('/home/sensei/bitstarter/index.html', 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
-  content = data;
+ app.get('/', function(request, response) {
+  response.send(data);
+});
+
 
 });
 
 
-app.get('/', function(request, response) {
-  response.send('content');
-});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
