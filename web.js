78a65d15c80884c3fa1ba570972acdf094a72a28
Amey Patil <theknight374121@gmail.com>
2013-07-10 02:07:40 +0530

    just javascript

--- web.js
+++ web.js
@@ -1,8 +1,4 @@
-var express = require('express');
-
-var app = express.createServer(express.logger());
-
-var fs  = require('fs');
+fs  = require('fs');
 fs.readFile('/home/sensei/bitstarter/index.html', 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
@@ -12,9 +8,3 @@ fs.readFile('/home/sensei/bitstarter/index.html', 'utf8', function (err,data) {
 
 });
 
-
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
